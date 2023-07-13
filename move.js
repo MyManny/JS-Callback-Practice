d1a38d0789a7444c99b8b69d24aac6a56ea96789
MyManny <gmail.com>
2023-07-13 14:55:06 -0700

    working?

--- move.js
+++ move.js
@@ -15,4 +15,26 @@ function move(element) {
         withArrowKeys: moveWithArrowKeys
     }        
 }
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+    callback(direction)
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+    callback(direction)
+})
 
